6386768689ea1ab43575dfa07eabcc3f8fdf899c
phebs <phebs@github.com>
2019-07-27 21:55:44 +1000

    Added dummy data

--- back-end/main.py
+++ back-end/main.py
@@ -7,6 +7,7 @@ from models.product import Product_Model
 from models.brand import Brand_Model
 from sustain_score import determine_score
 import json
+import random
 #Sets up api token and where it should be
 authorizations = {
     'apikey': {
@@ -45,9 +46,11 @@ product_model = api.model('product',{
     'product_name': fields.String,
     'product_industry': fields.String,
     'product_img': fields.String,
-    'brand_name': fields.String
+    'brand_name': fields.String,
+    'stock_level': fields.Integer,
+    'business_key': fields.String,
+    'price': fields.Integer,
 })
-
 
 
 parser = reqparse.RequestParser()
@@ -62,7 +65,9 @@ parser.add_argument('product_name', type=str, help='name', location='json')
 parser.add_argument('product_industry', type=str, help='name', location='json')
 parser.add_argument('product_img', type=str, help='name', location='json')
 parser.add_argument('brand_name', type=str, help='name', location='json')
-
+parser.add_argument('stock_level', type=int, help='name', location='json')
+parser.add_argument('business_key', type=str, help='name', location='json')
+parser.add_argument('price', type=int, help='name', location='json')
 
 
 
@@ -87,15 +92,6 @@ class Business(Resource):
         company = Business_Model.create_business(args)
         return {'message': 'Business has been created', 'business_key': company.key.urlsafe()}, 200
 
-
-@api.route('/create/stock')
-class Stock(Resource):
-    @api.doc(description="Creates the Stock profile")
-    @api.expect(stock_model)
-    def post(self):
-        args = parser.parse_args()
-        new_stock = Stock_Model.create_stock(args)
-        return {'message': 'Stock has been created','stock_key':new_stock.key.urlsafe() }, 200
 
 @api.route('/create/product')
 # "Resource" parameter is required simply due to implementation of Flask-Api (e.g. 'Must Have Just Cos')
@@ -129,6 +125,21 @@ class All_Product(Resource):
             dict_format.append(product.to_dict())
         return json.dumps(dict_format), 200
 
+
+'''
+Parses in the km radius
+Filters all the business in the area and returns all products in the area
+'''
+@api.route('/filter/<int:km>')
+class Filter_location(Resource):
+    def get(self, km):
+        return {},200
+#        parse to api- returns all the businesses in the area
+#         all_product = Product_Model.query(Product_Model.b == )
+
+
+
+
 @api.route('/create/brand')
 class Brand(Resource):
     @api.doc(description="Creates Brand") # Adds documentation
@@ -139,3 +150,73 @@ class Brand(Resource):
         Brand_Model.create_brand('Cooks Cuts', 4)
         Brand_Model.create_brand("Cadbury", 6)
         return {'message': 'Brand has been created'}, 200
+
+@api.route('/dummydata')
+class DummyData(Resource):
+    def get(self):
+        business_list = list()
+        business_data1 = {
+            'full_name': 'Vinnies',
+            'description': 'Best place to buy clothes',
+            'industry': 'Confectionary',
+            'suburb': 'Kingsford',
+            'address': '381 Anzac Pde.',
+            'post_code': 2032
+        }
+
+
+        business_data2 = {
+            'full_name': 'The Market',
+            'description': 'Best place to buy fresh vegetables',
+            'industry': 'Confectionary',
+            'suburb': 'Kingsford',
+            'address': '125A Houston Rd',
+            'post_code': 2032
+        }
+        business_data3 = {
+            'full_name': "Amber's Meat",
+            'description': 'Best place to buy Meat',
+            'industry': 'Meat',
+            'suburb': 'Kingsford',
+            'address': '12 Rainbow St',
+            'post_code': 2032
+        }
+        business_data4 = {
+            'full_name': 'Somedays Milk',
+            'description': 'Best place to buy yummy milk',
+            'industry': 'Dairy',
+            'suburb': 'Barangaroo',
+            'address': 'Shop 14/200 Barangaroo Avenue',
+            'post_code': 2000
+        }
+        business_list.append(Business_Model.create_business(business_data1))
+        business_list.append(Business_Model.create_business(business_data2))
+        business_list.append(Business_Model.create_business(business_data3))
+        business_list.append(Business_Model.create_business(business_data4))
+
+        brand_list = Brand_Model.query().fetch()
+        product_name_list = ["Chocolate", "Beef", "Milk Chocolate",  "Chicken","Celery","Carrot"]
+        product_image_list = ["https://images.app.goo.gl/dxYEv1M7wcGWzQnNA",
+                            "https://images.app.goo.gl/8RR6bDgxdyYKjxwQ9",
+                              "https://images.app.goo.gl/jk9WesvKDtxhUxU29",
+                              "https://images.app.goo.gl/fLsPZ5Y7SiQbxj2t8",
+                              "https://images.app.goo.gl/zx3hQLnJ9UAxkdkh6",
+                              "https://images.app.goo.gl/i5CxF32wA6s5x5TP8"
+                              ]
+        length_product = len(product_name_list) - 1
+
+        for i in range(20):
+            brand = random.choice(brand_list)
+            business = random.choice(business_list)
+            score = determine_score(brand.score, business.industry)
+            product_index = random.randint(0,length_product)
+            product_data = {
+                "product_name": product_name_list[product_index],
+                "product_industry": business.industry,
+                "product_img": product_image_list[product_index],
+                "stock_level": random.randint(1,50),
+                "business_key": business.key.urlsafe(),
+                "price": random.randint(1,20)
+            }
+            Product_Model.create_product(product_data, score, brand)
+        return {'message': 'Completed'},200
--- back-end/models/product.py
+++ back-end/models/product.py
@@ -7,17 +7,23 @@ class Product_Model(ndb.Model):
     product_img = ndb.StringProperty() # Stores Image link as string
     brand_key= ndb.StringProperty()
     score = ndb.FloatProperty()
+    level = ndb.IntegerProperty()
+    price = ndb.IntegerProperty()
+    business_key = ndb.StringProperty()
+
 
     """ Initialises Product Object """
     @staticmethod
     def create_product(product_data, score,brand):
         product = Product_Model()
-        print(product_data.get("product_name"))
         product.product_name = product_data.get("product_name")
         product.product_industry = product_data.get("product_industry")
         product.product_img = product_data.get("product_img")
         product.brand_key = brand.key.urlsafe()
         product.score = score
+        product.level = product_data.get("stock_level")
+        product.business_key = product_data.get("business_key")
+        product.price = product_data.get("price")
         product.put()
         return product
 
