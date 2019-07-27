45c93b58218f04208814f4ad5446e159a14255cf
joanne-li <40012481+joanne-li@users.noreply.github.com>
2019-07-28 03:36:55 +1000

    Issues with merge

--- frontend/src/ProductList.js
+++ frontend/src/ProductList.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
 //import { Route } from 'react-router-dom';
-import {Grid, Image, Segment} from 'semantic-ui-react'
+import {Grid, Image, Segment, Select, Container, Form, Button} from 'semantic-ui-react'
 import axios from 'axios'
 import Item from './Item.js'
-import Dashboard from './Dashboard'
+import Banner from './Banner';
+
+
+const sortOptions = [
+  {key: 'abc', value: 'Alphabetical', text: 'Alphabetical'},
+  {key: 'p123', value: 'Price (Ascending)', text: 'Price (Ascending)'},
+  {key: 'p321', value: 'Price (Descending)', text: 'Price (Descending)'},
+  {key: 's123', value: 'Sustainability (Ascending)', text: 'Sustainability (Ascending)'},
+  {key: 's321', value: 'Sustainability (Descending)', text: 'Sustainability (Descending)'}
+];
 
 
 class ProductList extends Component {
   constructor(){
     super();
-    this.state = { products: [], productState: []}
+    this.state = { products: [] } ;
+    this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount(){
-    axios.get('https://inspired-victor-144517.appspot.com')
+    axios.get('https://inspired-victor-144517.appspot.com/product')
       .then((result) => {
-          this.setState({products: result.data.products})
+          this.setState({ products: result.data });
             }
-          );
+          ).catch(err => console.log(err));
   }
-
+  handleClick (e) {
+    e.preventDefault();
+    axios.get('https://inspired-victor-144517.appspot.com/product')
+      .then((result) => {
+        this.setState({products: result.data});
+      });
+  }
   render() {
     var productItems = this.state.products.map((item) => {
       return (
+        <div>
           <Grid.Column>
-          <Item name={item['name']} price={item['price']} business={item['business']} sustainability={item['sustainability']} img={item['img']}/>
+          <Item name={item['product_name']}
+            price={item['price']}
+            industry={item['product_industry']}
+            brand={item['brand_key']}
+            business={item['business_key']}
+            stocklevel={item['level']}
+            sustainability={item['score']}
+            img={item['product_img'] }/>
           </Grid.Column>
+          </div>
       )});
 
 
     return (
       <div>
-<<<<<<< HEAD
-=======
         <Banner />
       <Container>
       <Segment raised>
@@ -42,10 +65,22 @@ class ProductList extends Component {
         <Form.Button onClick={this.handleClick}>Submit</Form.Button>
       </Form>
       </Segment>
->>>>>>> d1c540b0204cc160a19a6fc07960d5a3964e7913
       <Grid container columns={4}>
-        {productItems}
+        {this.state.products.map((item) => {
+          return (
+              <Grid.Column>
+              <Item name={item['product_name']}
+                price={item['price']}
+                industry={item['product_industry']}
+                brand={item['brand_key']}
+                business={item['business_key']}
+                stocklevel={item['level']}
+                sustainability={item['score']}
+                img={item['product_img'] }/>
+              </Grid.Column>
+          )})}
       </Grid>
+      </Container>
       </div>
     );
   }
@@ -53,3 +88,4 @@ class ProductList extends Component {
 
 
 export default ProductList;
+
