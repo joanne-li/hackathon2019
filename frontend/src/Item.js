0578236d291129eae77bc039b182853781f5eded
Joanne <joanne.li3@outlook.com>
2019-07-27 14:19:44 +1000

    own package

---
+++ frontend/src/Item.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+// Need to get picture of item from img folder
+// but this is handled from backend
+
+class Item extends Component {
+  render() {
+    return (
+      <div className="Item">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
